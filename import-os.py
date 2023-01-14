cf62592da7e95121a829b0596a93398629620be5
alisdairdaniel <117979215+alisdairdaniel@users.noreply.github.com>
2023-01-14 00:06:35 -0800

    Update import-os.py

--- import-os.py
+++ import-os.py
@@ -4,10 +4,11 @@ targetDir = r'd:\tmp\util\dist\room'
 files = []
 dirs  = []
 
+def dir_how():
+    for (dirpath, dirnames, filenames) in os.walk(targetDir):
+        files += filenames
+        dirs += dirnames
 
-for (dirpath, dirnames, filenames) in os.walk(targetDir):
-    files += filenames
-    dirs += dirnames
-
-print(files)
-print(dirs)
+    print(files)
+    print(dirs)
+aaa = dir_how()
